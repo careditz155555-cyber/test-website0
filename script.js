a942a750dbe1dc906d0cb5f03e892342fbe94d60
careditz155555-cyber <careditz155555@gmail.com>
2026-02-14 22:16:51 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+document.querySelector("h1").addEventListener("click", function() {
+    alert("You clicked the heading!");
+});
+document.getElementById("colorBtn").addEventListener("click", function() {
+    document.body.style.backgroundColor = "purple";
+});
