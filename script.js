f1109e23e2537b0771453cd5a5740215a30bde51
careditz155555-cyber <careditz155555@gmail.com>
2026-03-17 18:21:29 -0400

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-document.querySelector("h1").addEventListener("click", function() {
-    alert("You clicked the heading!");
-});
-document.getElementById("colorBtn").addEventListener("click", function() {
-    document.body.style.backgroundColor = "purple";
-});
